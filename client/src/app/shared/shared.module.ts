b850d589398321459d2bdeeefdf21535377788f3
Max <maxim-7x@mail.ru>
2023-09-06 11:55:52 +0300

    updated basket client

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,22 +4,17 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './paging-header/paging-header.component';
 import { PagerComponent } from './pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [
-    PagingHeaderComponent,
-    PagerComponent
-  ],
-  imports: [
-    CommonModule,
-    PaginationModule.forRoot(),
-    CarouselModule.forRoot()
-  ],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
+  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
-    CarouselModule
-  ]
+    CarouselModule,
+    OrderTotalsComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
